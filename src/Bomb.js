5f73cbee6e4e9ed7c1448985230efd10c1128c0f
jennifer-yoo <jennifer.yoo7@gmail.com>
2020-09-14 18:34:24 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,28 @@
-// your Bomb code here!
+import React from 'react'
+
+export default class Bomb extends React.Component {
+
+    constructor(currentSlideIndex) {
+        super()
+        this.state = {
+            secondsLeft: currentSlideIndex.initialCount
+        }
+    }
+
+    // timer = () => {
+    //     if (this.state.secondsLeft === 0) {
+    //         'Boom!'
+    //     } else {
+    //         `${this.state.secondsLeft} seconds left before I go boom!`
+    //     }
+    // }
+    
+    render() {
+        const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`
+
+        return (
+            <h2>{message}</h2>
+        )
+    }
+
+}
